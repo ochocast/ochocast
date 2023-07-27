38f1f638b164d3dda34d6c91717170acf2793da8
Samuel Fouchard <sam.bloom2001@gmail.com>
2023-07-27 10:13:02 +0200

    login page

---
+++ octocast/src/presentation/components/HeaderComponent/UserButtonComponent/UserButton.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import './UserButton.css';
+
+import userIcon from '../../../assets/UserIcon.png'
+
+interface HeaderUserButtonProps {}
+
+const HeaderUserButton: FC<HeaderUserButtonProps> = () => (
+  <div className="HeaderUserButton">
+    <span className='TextBox'>Utilisateur</span>
+    <img className='IconLogo' src={userIcon} alt='IconLogo'></img>
+  </div>
+);
+
+export default HeaderUserButton;
