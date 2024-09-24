5b1e2fed457bd1a3eb95682a9ef453a548676b4b
Gabriel Soufflet <75330112+SouGab@users.noreply.github.com>
2024-09-24 19:59:01 +0200

    Feat: Loading Page (#113)

---
+++ frontend/src/components/newComponents/LoadingCircle/LoadingCircle.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LoadingCircle.css';
+
+const LoadingCircle = () => {
+    return(
+        <div>
+            <div className="loader-container">
+                <div className="loader"></div>
+            </div>
+        </div>
+    );
+};
+
+export default LoadingCircle;
--- frontend/src/pages/videoMedia/videoMedia.tsx
+++ frontend/src/pages/videoMedia/videoMedia.tsx
@@ -8,26 +8,34 @@ import { getMedia, getVideo} from '../../utils/api';
 import { useParams } from 'react-router-dom';
 import { Video } from '../../utils/VideoProperties';
 import NotFoundPage from '../notFound/notFound';
+import LoadingCircle from '../../components/newComponents/LoadingCircle/LoadingCircle';
 const ReactPlayer = _ReactPlayer as unknown as React.FC<ReactPlayerProps>;
 
 interface VideoMediaProps{}
 
 const VideoMedia: FC<VideoMediaProps> = () => {
   const { videoId }= useParams();
-  const [url, setUrl] = useState<string>();
+  const [url, setUrl] = useState<string>('');
   const [video, setVideo] = useState<Video>();
+  const [isLoading, setIsLoading] =  useState(false);
 
   useEffect(() => {
     const getMe = async () => {
+      setIsLoading(true);
       const video_response = await getVideo(videoId);
       const url_response = await getMedia(videoId);
       if (url_response != undefined)
         setUrl(url_response.data);
       if(video_response != undefined)
         setVideo(video_response.data[0]);
+      setIsLoading(false);
     };
     getMe();
   }, [videoId]);
+
+  if (isLoading){
+    return LoadingCircle();
+  }
 
   if(video != undefined)
     return (
