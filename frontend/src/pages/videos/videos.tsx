44b3775675302992d7955dd315cf8c34789ea031
OrianeMarg <159463087+OrianeMarg@users.noreply.github.com>
2024-09-24 20:17:56 +0200

    Oct 212 search bar for videos (#108)
    * feat: searchbar
    
    * feat: searchbar is working
    
    ---------
    
    Co-authored-by: Oriane Margelisch <oriane.margelisch@epita.fr>
    Co-authored-by: humainC <116105016+humainC@users.noreply.github.com>

--- frontend/src/pages/videos/videos.tsx
+++ frontend/src/pages/videos/videos.tsx
@@ -1,48 +1,63 @@
 import React, { useEffect } from 'react';
-import { useState}  from 'react';
+import { useState } from 'react';
 import './videos.css';
 import { FC } from 'react';
 import { getVideos } from '../../utils/api';
 import { Video } from '../../utils/VideoProperties';
 import VideosList from '../../components/newComponents/VideosList/VideosList';
+import SearchBar from '../../components/newComponents/SearchBar/SearchBar';
 import Button from '../../components/buttons/button/button';
 import { useNavigate } from 'react-router-dom';
 
 
-interface VideosProps{}
+interface VideosProps {}
 
 const Videos: FC<VideosProps> = () => {
-  const [videos, setVideo] = useState<Video[]>();
+  const [videos, setVideo] = useState<Video[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
   const userString = localStorage.getItem('backendUser');
   const navigate = useNavigate();
 
 
   const getMe = async () => {
-    const videos_response = await getVideos();
-    setVideo(videos_response.data);
+    try {
+      const videos_response = await getVideos();
+      setVideo(videos_response.data || []);
+    } catch (error) {
+      console.error('Error fetching videos:', error);
+    }
   };
 
   useEffect(() => {
     getMe();
   }, [userString]);
 
+  const handleSearch = (term: string) => {
+    console.log('Search term:', term);
+    setSearchTerm(term);
+  };
+
+  // Filtrage des vidéos basé sur le terme de recherche
+  const filteredVideos = videos.filter((video) =>
+    video.title.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+  console.log('Filtered Videos:', filteredVideos);
+
   return (
     <div className="videos">
       <div className="button-event-creation">
         <Button onClick={() => navigate('/video/video-settings')}>Uploader une vidéo</Button>
       </div>
-      <div className='content'>
-        {(videos && videos.length != 0) ?
-        <VideosList
-            title="Vidéos Publiées"
-            videos={videos}
-          />:
+      <div className="content">
+        <SearchBar onSearch={handleSearch} />
+        {filteredVideos.length > 0 ? (
+          <VideosList title="Vidéos Publiées" videos={filteredVideos} />
+        ) : (
           <h1>No Video Found</h1>
-        }
+        )}
       </div>
     </div>
   );
 };
 
 export default Videos;
-
