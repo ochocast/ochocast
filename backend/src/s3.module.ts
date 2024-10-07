b8efc76207e42e8fdfdd85772e6eac444d70e1ff
humainC <116105016+humainC@users.noreply.github.com>
2024-10-07 12:05:03 +0200

    Oct 258 je peux stocker mes videos sur le bucket scaleway (#120)
    * feat: tried to update .env and s3 module to reach prod s3 bucket, didn t work for this time
    
    * feat: on peut reach la prod depuis le local
    
    * feat: changed CI CD to make it work in prod
    
    * feat: hidden creds
    
    * feat: clean dead code in s3module
    
    ---------
    
    Co-authored-by: leo.verniquet <leo.verniquet@epita.fr>

--- backend/src/s3.module.ts
+++ backend/src/s3.module.ts
@@ -2,15 +2,17 @@ import { Injectable, Module } from '@nestjs/common';
 import { S3Client } from "@aws-sdk/client-s3";
 
 
+// Configuration du client S3 pour Scaleway
 const s3Client = new S3Client({
-  region: "eu-west",
-  endpoint: process.env.STOCK_SERVER_URL,
+  region: process.env.STOCK_REGION, // Utilise la région Scaleway
+  endpoint: process.env.STOCK_SERVER_URL, // Endpoint Scaleway
   credentials: {
-    accessKeyId: process.env.STOCK_CLIENT_ID,
-    secretAccessKey: process.env.STOCK_SECRET,
+    accessKeyId: process.env.STOCK_CLIENT_ID, // Clé d'accès depuis les variables d'environnement
+    secretAccessKey: process.env.STOCK_SECRET, // Secret d'accès depuis les variables d'environnement
   },
-  forcePathStyle: true,
+  forcePathStyle: true, // Obligatoire pour compatibilité avec Scaleway
 });
+
 
 @Module({
   providers: [
