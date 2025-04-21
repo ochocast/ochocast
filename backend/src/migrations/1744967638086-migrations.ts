f86b152a6ba8d2f7556fc7ef106a162210c4f61e
TitouanGragnic <97364019+TitouanGragnic@users.noreply.github.com>
2025-04-21 11:34:20 +0200

    Update 1744967638086-migrations.ts

--- backend/src/migrations/1744967638086-migrations.ts
+++ backend/src/migrations/1744967638086-migrations.ts
@@ -4,11 +4,34 @@ export class Migrations1744967638086 implements MigrationInterface {
     name = 'Migrations1744967638086'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "event_entity" DROP COLUMN "category"`);
+        await queryRunner.query(`
+            DO $$
+            BEGIN
+                IF EXISTS (
+                    SELECT 1
+                    FROM information_schema.columns
+                    WHERE table_name='event_entity' AND column_name='category'
+                ) THEN
+                    ALTER TABLE "event_entity" DROP COLUMN "category";
+                END IF;
+            END
+            $$;
+        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "event_entity" ADD "category" character varying NOT NULL`);
+        await queryRunner.query(`
+            DO $$
+            BEGIN
+                IF NOT EXISTS (
+                    SELECT 1
+                    FROM information_schema.columns
+                    WHERE table_name='event_entity' AND column_name='category'
+                ) THEN
+                    ALTER TABLE "event_entity" ADD "category" character varying NOT NULL;
+                END IF;
+            END
+            $$;
+        `);
     }
-
 }
