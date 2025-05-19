05c4e18926ac9cc09f54e94924e9474848bcfbbd
LordAzeth <77928771+LordAzeth@users.noreply.github.com>
2025-05-19 20:50:51 +0200

    [OCT-376] feat: rework front video (#444)
    * feat: change tags position and add creationdate
    
    * feat: add view icon
    
    * feat : Modify icon to lucid icon + add button : 'Archiver', 'Modifier'
    
    * [OCT-376]fix: modify button now works
    
    ---------
    
    Co-authored-by: LordAzeth <loan.bernable@epita.fr>
    Co-authored-by: root <root@DESKTOP-UBVFEIO>
    Co-authored-by: louis mirallie <louis.mirallie@epita.fr>

--- frontend/src/pages/videoMedia/videoMedia.tsx
+++ frontend/src/pages/videoMedia/videoMedia.tsx
@@ -5,7 +5,7 @@ import { default as _ReactPlayer } from 'react-player/lazy';
 import { ReactPlayerProps } from 'react-player/types/lib';
 import { FC } from 'react';
 import { getMedia, getVideo } from '../../utils/api';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Video } from '../../utils/VideoProperties';
 import NotFoundPage from '../notFound/notFound';
 import LoadingCircle from '../../components/ReworkComponents/LoadingCircle/LoadingCircle';
@@ -14,6 +14,9 @@ import ProfileDescription, {
   ProfileDescriptionState,
 } from '../../components/ReworkComponents/profil/ProfileDescription/ProfileDescription';
 import Card from '../../components/ReworkComponents/generic/Cards/Card';
+import Vues from '../../../src/assets/vues.svg';
+import Button, {ButtonType} from '../../components/ReworkComponents/generic/Button/Button';
+import { useTranslation } from 'react-i18next';
 // import Thumbnail from '../../components/ReworkComponents/video/Thumbnail/Thumbnail';
 // import Commentary from '../../components/ReworkComponents/Commentary/Commentary';
 const ReactPlayer = _ReactPlayer as unknown as React.FC<ReactPlayerProps>;
@@ -36,6 +39,7 @@ interface VideoMediaProps {}
 // ];
 
 const VideoMedia: FC<VideoMediaProps> = () => {
+  const { t } = useTranslation();
   const { videoId } = useParams();
   const [url, setUrl] = useState<string>('');
   const [video, setVideo] = useState<Video>();
@@ -43,6 +47,7 @@ const VideoMedia: FC<VideoMediaProps> = () => {
   // Durée d'expiration en secondes, doit etre equivalent a la durée mise en backend
   const linkExpirationTime = 3600;
   const renewalThreshold = 300;
+  const navigate = useNavigate();
 
   const renewSignedUrl = async () => {
     const url_response = await getMedia(videoId);
@@ -72,12 +77,36 @@ const VideoMedia: FC<VideoMediaProps> = () => {
   if (video != undefined)
     return (
       <div className={styles.containerGlobal}>
-        <h2 className={styles.video_title}>{video?.title}</h2>
-        {/* <h4 className={styles.tagList}>
-          Tag :
-          <Tag content="Devops" />
-          <Tag content="Docker" />
-        </h4> */}
+        <h2 className={styles.video_title}>
+        {video?.title}
+        &nbsp;&nbsp;
+        
+        <span className={styles.vues}>
+          <img className={styles.icons} src={Vues} alt="Vue icon"/>
+          &nbsp;
+          {video?.views}
+          </span>
+        </h2>
+
+        
+        <h4 className={styles.tagList}>
+          Tags :
+          {video.tags && video.tags.map((tag, id) => <Tag key={id} content={tag.name} />)}
+        </h4>
+
+        <div className={styles.buttonList}>
+          <Button
+            type={ButtonType.secondary}
+            label={t('archiveVideo')}
+            //onClick={() => video.onArchived!(video.id)}
+          />
+        
+          <Button
+            type={ButtonType.primary}
+            label={t('modifyVideo')}
+            onClick={() => navigate(`/video/video-settings/${video.id}`)}
+          />
+          </div>
 
         <div className={styles.containerPlayer}>
           <div className={styles.videoPlayer}>
@@ -93,16 +122,19 @@ const VideoMedia: FC<VideoMediaProps> = () => {
             />
           </div>
           <div className={styles.containerPlayerRight}>
-            <div>
-              <h3 className={styles.video_title}>
-                <h4 className={styles.tagList}>
-                  Tag :
-                  {video.tags &&
-                  video.tags.map((tag, id) => <Tag key={id} content={tag.name} />)}
-                </h4>
+            <div className={styles.video_information}>
+              <h3 className={styles.video_description_title}>
                 Description :
               </h3>
               <p className={styles.video_description}>{video?.description}</p>
+              
+              <h3 className={styles.video_date}>
+                Publié le :
+              <br/> 
+                {video?.createdAt.toString()[8]}{video?.createdAt.toString()[9]}/
+                {video?.createdAt.toString()[5]}{video?.createdAt.toString()[6]}/
+                {video?.createdAt.toString()[0]}{video?.createdAt.toString()[1]}{video?.createdAt.toString()[2]}{video?.createdAt.toString()[3]}
+              </h3>
             </div>
             <div className={styles.profileDescription}>
               <ProfileDescription
